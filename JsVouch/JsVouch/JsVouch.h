7cb66acbb2b551322ed548c1e9a7e782b7b5fe64
sharjeelahmed <sharjeelahmed92@hotmail.com>
2020-06-10 23:37:50 +0500

    Project initialization

---
+++ JsVouch/JsVouch/JsVouch.h
@@ -0,0 +1,19 @@
+//
+//  JsVouch.h
+//  JsVouch
+//
+//  Created by Shairjeel Ahmed on 10/06/2020.
+//  Copyright © 2020 Shairjeel Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JsVouch.
+FOUNDATION_EXPORT double JsVouchVersionNumber;
+
+//! Project version string for JsVouch.
+FOUNDATION_EXPORT const unsigned char JsVouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsVouch/PublicHeader.h>
+
+
